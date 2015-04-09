5829c480326bd2b829a21dc4b38039f4b3bd6058
Jaden Geller <jgeller@caltech.edu>
2015-04-09 02:04:34 -0700

    Initial project

---
+++ Helium/Helium/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Helium
+//
+//  Created by Jaden Geller on 4/9/15.
+//  Copyright (c) 2015 Jaden Geller. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
