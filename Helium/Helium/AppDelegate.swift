4a9f18cfbffad749b7b3cf4d6719ccd5c8a86393
Jaden Geller <jgeller@caltech.edu>
2015-04-09 02:04:34 -0700

    Initial project

---
+++ Helium/Helium/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Helium
+//
+//  Created by Jaden Geller on 4/9/15.
+//  Copyright (c) 2015 Jaden Geller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
