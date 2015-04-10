a0afc753228197030efc239f052927124d463bbc
Jaden Geller <jgeller@caltech.edu>
2015-04-09 21:20:10 -0700

    Made the first time user experience a bit friendlier

--- Helium/Helium/WebViewController.swift
+++ Helium/Helium/WebViewController.swift
@@ -33,7 +33,7 @@ class WebViewController: NSViewController {
         webView.mainFrame.reload()
     }
     func clear() {
-        webView.mainFrame.loadHTMLString("<!DOCTYPE html><html><head><style>body{background-color:#101010}</style></head></html>", baseURL: nil)
+        webView.mainFrame.loadHTMLString("<!DOCTYPE html><html><head><style>body{background-color:#3399FF; color:white; font-family: 'Helvetica Neue'}</style></head><body><center>Navigate to a webpage using the <b>Location</b> menu in the menubar.<br><br><img src='http://jadengeller.github.io/Helium/helium_icon.png' style='width:45%'></center></body></html>", baseURL: nil)
     }
 
     @IBOutlet weak var webView: WebView!
