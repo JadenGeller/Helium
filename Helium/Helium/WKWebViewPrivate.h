ac6c7f1449f29559610dff828c92714eed385460
Jaden Geller <jgeller@caltech.edu>
2015-04-13 04:12:23 -0700

    updated to use WKWebView
    unfortunately uses a private method to set user agent

---
+++ Helium/Helium/WKWebViewPrivate.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  Helium
+//
+//  Created by Jaden Geller on 4/13/15.
+//  Copyright (c) 2015 Jaden Geller. All rights reserved.
+//
+
+@import WebKit;
+
+@interface WKWebView (Privates)
+
+@property (copy, setter=_setCustomUserAgent:) NSString *_customUserAgent;
+
+@property (nonatomic, readonly) NSString *_userAgent;
+
+@end
