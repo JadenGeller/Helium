789ec04dfc14e635fcf924c84ad6e60836b9908b
Jaden Geller <jgeller@caltech.edu>
2015-04-13 08:08:16 -0700

    changed version number

--- Helium/Helium/AppDelegate.swift
+++ Helium/Helium/AppDelegate.swift
@@ -7,16 +7,39 @@
 //
 
 import Cocoa
+//import Squirrel
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var magicURLMenu: NSMenuItem!
     
+//    var updater: SQRLUpdater!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         magicURLMenu.state = NSUserDefaults.standardUserDefaults().boolForKey("disabledMagicURLs") ? NSOffState : NSOnState
         
+        // Autoupdate code
+        
+//        let components = NSURLComponents()
+//        components.scheme = "https";
+//        components.host = "gist.githubusercontent.com";
+//        components.path = "/JadenGeller/321e2319e288e17f59aa/raw/10c22987095377f35f55824e7a5de2cc596db665/helum_update";
+//        
+//        let bundleVersion = NSBundle.mainBundle().sqrl_bundleVersion
+//        components.query = "version=\(bundleVersion)".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLFragmentAllowedCharacterSet())
+//        
+//        updater = SQRLUpdater(updateRequest: NSURLRequest(URL: components.URL!))
+//        
+//        // Check for updates every 4 hours.
+//        self.updater.startAutomaticChecksWithInterval(60 * 60 * 4)
+//        
+//        self.updater.updates.subscribeNext {
+//            let downloadedUpdate = $0 as! SQRLDownloadedUpdate
+//            println(downloadedUpdate)
+//        }
+
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
