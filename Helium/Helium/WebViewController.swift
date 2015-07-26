998423cbe611252e87a7eb3c9e8dece6d0ecab3c
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2015-07-26 22:15:13 +0300

    Changed user agent string

--- Helium/Helium/WebViewController.swift
+++ Helium/Helium/WebViewController.swift
@@ -25,7 +25,7 @@ class WebViewController: NSViewController, WKNavigationDelegate {
         webView.configuration.preferences.plugInsEnabled = true
         
         // Netflix support via Silverlight (HTML5 Netflix doesn't work for some unknown reason)
-        webView._customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/600.5.17 (KHTML, like Gecko) Version/7.1.5 Safari/537.85.14"
+        webView._customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
         
         // Setup magic URLs
         webView.navigationDelegate = self
