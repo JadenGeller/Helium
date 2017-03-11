d3c17eeee78b90fd7c5b67de7a10e1f5eee1fa43
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-03-11 16:39:14 +0900

    copy and pastable NSField

--- Helium/Helium/HeliumPanelController.swift
+++ Helium/Helium/HeliumPanelController.swift
@@ -10,6 +10,40 @@ import AppKit
 
 let optionKeyCode: UInt16 = 58
 
+// editable NSTextField
+class Editing: NSTextField {
+    private let commandKey = NSEventModifierFlags.command.rawValue
+    private let commandShiftKey = NSEventModifierFlags.command.rawValue | NSEventModifierFlags.shift.rawValue
+
+    override func performKeyEquivalent(with event: NSEvent) -> Bool {
+        if event.type == NSEventType.keyDown {
+            if (event.modifierFlags.rawValue &
+                NSEventModifierFlags.deviceIndependentFlagsMask.rawValue) == commandKey {
+                switch event.charactersIgnoringModifiers! {
+                case "x":
+                    if NSApp.sendAction(#selector(NSText.cut(_:)), to:nil, from:self) { return true }
+                case "c":
+                    if NSApp.sendAction(#selector(NSText.copy(_:)), to:nil, from:self) { return true }
+                case "v":
+                    if NSApp.sendAction(#selector(NSText.paste(_:)), to:nil, from:self) { return true }
+                case "z":
+                    if NSApp.sendAction(Selector(("undo:")), to:nil, from:self) { return true }
+                case "a":
+                    if NSApp.sendAction(#selector(NSResponder.selectAll(_:)), to:nil, from:self) { return true }
+                default:
+                    break
+                }
+            }
+            else if (event.modifierFlags.rawValue & NSEventModifierFlags.deviceIndependentFlagsMask.rawValue) == commandShiftKey {
+                if event.charactersIgnoringModifiers == "Z" {
+                    if NSApp.sendAction(Selector(("redo:")), to:nil, from:self) { return true }
+                }
+            }
+        }
+        return super.performKeyEquivalent(with: event)
+    }
+}
+
 class HeliumPanelController : NSWindowController {
     let userDefaults = UserDefaults.standard
 
@@ -278,14 +312,13 @@ class HeliumPanelController : NSWindowController {
             }
         }
     }
-    
-    
+
     private func didRequestLocation() {
         let alert = NSAlert()
         alert.alertStyle = NSAlertStyle.informational
         alert.messageText = "Enter Destination URL"
-        
-        let urlField = NSTextField()
+
+        let urlField = Editing()
         urlField.frame = NSRect(x: 0, y: 0, width: 300, height: 20)
         urlField.lineBreakMode = NSLineBreakMode.byTruncatingHead
         urlField.usesSingleLineMode = true
