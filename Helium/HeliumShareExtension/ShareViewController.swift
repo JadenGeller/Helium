ec9b9097529665862f16d55e4352c4361a83ec35
Jaden Geller <jaden.geller@gmail.com>
2020-04-19 15:20:08 -0700

    Convert Share extension to Swift 5

--- Helium/HeliumShareExtension/ShareViewController.swift
+++ Helium/HeliumShareExtension/ShareViewController.swift
@@ -17,32 +17,32 @@ class ShareViewController: NSViewController {
 	override func viewDidLoad() {
 		
 		if let item = self.extensionContext!.inputItems.first as? NSExtensionItem,
-			let attachment = item.attachments?.first as? NSItemProvider
-			where attachment.hasItemConformingToTypeIdentifier("public.url")
+            let attachment = item.attachments?.first,
+            attachment.hasItemConformingToTypeIdentifier("public.url")
 		{
-			attachment.loadItemForTypeIdentifier("public.url", options: nil)
+            attachment.loadItem(forTypeIdentifier: "public.url", options: nil)
 				{
 					(url, error) in
 					
-					if let url = url as? NSURL,
-						let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
+					if let url = url as? URL,
+                        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
 					{
 						
 						components.scheme = "helium"
 						
-						let heliumURL = components.URL!
+						let heliumURL = components.url!
 						
-						NSWorkspace.sharedWorkspace().openURL( heliumURL )
+						NSWorkspace.shared.open(heliumURL)
 					}
 					
 			}
 			
-			self.extensionContext!.completeRequestReturningItems(nil, completionHandler: nil)
+            self.extensionContext!.completeRequest(returningItems: nil, completionHandler: nil)
 			return
 		}
 		
-		let error = NSError(domain: NSCocoaErrorDomain, code: NSURLErrorBadURL, userInfo: nil)
-		self.extensionContext!.cancelRequestWithError(error)
+        let error = NSError(domain: NSCocoaErrorDomain, code: URLError.badURL.rawValue, userInfo: nil)
+        self.extensionContext!.cancelRequest(withError: error)
 	}
 	
 	@IBAction func send(sender: AnyObject?) {
@@ -50,12 +50,12 @@ class ShareViewController: NSViewController {
 		// Complete implementation by setting the appropriate value on the output item
 		
 		let outputItems = [outputItem]
-		self.extensionContext!.completeRequestReturningItems(outputItems, completionHandler: nil)
+        self.extensionContext!.completeRequest(returningItems: outputItems, completionHandler: nil)
 	}
 	
 	@IBAction func cancel(sender: AnyObject?) {
 		let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
-		self.extensionContext!.cancelRequestWithError(cancelError)
+        self.extensionContext!.cancelRequest(withError: cancelError)
 	}
 	
 }
